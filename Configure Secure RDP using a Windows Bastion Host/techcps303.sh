8eb0007cbaa4c37dd40466fc1a60aae02bb541df
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-17 03:57:53 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Configure Secure RDP using a Windows Bastion Host/techcps303.sh
+++ Configure Secure RDP using a Windows Bastion Host/techcps303.sh
@@ -1,14 +1,5 @@
-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-export ZONE=
-
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-REGION="${ZONE%-*}"
+export REGION="${ZONE%-*}"
 
 gcloud compute networks create securenetwork --project=$DEVSHELL_PROJECT_ID --subnet-mode=custom --mtu=1460 --bgp-routing-mode=regional && gcloud compute networks subnets create secure-subnet --project=$DEVSHELL_PROJECT_ID --range=10.0.0.0/24 --stack-type=IPV4_ONLY --network=securenetwork --region=$REGION
 
@@ -18,18 +9,3 @@ gcloud compute instances create vm-securehost --project=$DEVSHELL_PROJECT_ID --z
 
 gcloud compute instances create vm-bastionhost --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-small --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=secure-subnet --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --tags=rdp --create-disk=auto-delete=yes,boot=yes,device-name=vm-securehost,image=projects/windows-cloud/global/images/windows-server-2016-dc-v20230510,mode=rw,size=150,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-standard --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
-
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-gcloud compute reset-windows-password vm-securehost --user app_admin --zone $ZONE --quiet
-
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-                          Congratulations, you're all done with the lab 😄Don't forget to subscribe our YouTube Channel😄
-                                        
-                                                                 Thank You!!!!
--------------------------------------------------------------------------------------------------------------------------------------------------------------------
