9aacedbf33c88cf345ae4c32c00a0230a91cf8f6
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-18 20:55:34 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/techcps1.sh
+++ Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/techcps1.sh
@@ -1,16 +1,9 @@
 
 
 export AR_REPO='chef-repo'
-
 export SERVICE_NAME='chef-streamlit-app'
-
 gcloud artifacts repositories create $AR_REPO --repository-format=docker --location=$REGION
-
-
 gcloud builds submit --tag="$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/$AR_REPO/$SERVICE_NAME"
-
-
-gcloud run deploy "$SERVICE_NAME" --image="$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/$AR_REPO/$SERVICE_NAME" --port=8080 --platform=managed --region=$REGION --project=$DEVSHELL_PROJECT_ID --set-env-vars=PROJECT=$DEVSHELL_PROJECT_ID,REGION=$REGION --allow-unauthenticated
 
 
 gcloud run deploy "$SERVICE_NAME" \
