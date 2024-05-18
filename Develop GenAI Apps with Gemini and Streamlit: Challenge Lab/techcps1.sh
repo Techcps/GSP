2821d035aa7bb0270ead738b5674c75280814af5
Techcps <104138529+Techcps@users.noreply.github.com>
2024-05-18 21:44:08 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/techcps1.sh
+++ Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/techcps1.sh
@@ -1,17 +1,17 @@
 
+export PROJECT=$DEVSHELL_PROJECT_ID
 
-export AR_REPO='chef-repo'
-export SERVICE_NAME='chef-streamlit-app'
-gcloud artifacts repositories create $AR_REPO --repository-format=docker --location=$REGION
-gcloud builds submit --tag="$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/$AR_REPO/$SERVICE_NAME"
+AR_REPO='chef-repo'
+SERVICE_NAME='chef-streamlit-app' 
+gcloud artifacts repositories create "$AR_REPO" --repository-format=Docker --location="$REGION"
+gcloud builds submit --tag "$REGION-docker.pkg.dev/$PROJECT/$AR_REPO/$SERVICE_NAME"
 
 
 gcloud run deploy "$SERVICE_NAME" \
   --port=8080 \
-  --image="$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/$AR_REPO/$SERVICE_NAME" \
+  --image="$REGION-docker.pkg.dev/$PROJECT/$AR_REPO/$SERVICE_NAME" \
   --allow-unauthenticated \
   --region=$REGION \
   --platform=managed  \
-  --project=$DEVSHELL_PROJECT_ID \
-  --set-env-vars=GCP_PROJECT=$DEVSHELL_PROJECT_ID,GCP_REGION=$REGION
-
+  --project=$PROJECT \
+  --set-env-vars=GCP_PROJECT=$PROJECT,GCP_REGION=$REGION
