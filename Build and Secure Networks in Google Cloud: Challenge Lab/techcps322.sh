ed1035cf88b2322130616727a00f6215911e76e3
Techcps <104138529+Techcps@users.noreply.github.com>
2024-06-04 08:17:36 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Build and Secure Networks in Google Cloud: Challenge Lab/techcps322.sh
@@ -0,0 +1,22 @@
+
+
+gcloud compute firewall-rules delete open-access --quiet
+ 
+
+gcloud compute firewall-rules create ssh-ingress --allow=tcp:22 --source-ranges 35.235.240.0/20 --target-tags $IAP_NETWORK_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags bastion --tags=$IAP_NETWORK_TAG --zone=$ZONE
+ 
+
+gcloud compute firewall-rules create http-ingress --allow=tcp:80 --source-ranges 0.0.0.0/0 --target-tags $HTTP_NETWORK_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags juice-shop --tags=$HTTP_NETWORK_TAG --zone=$ZONE
+ 
+
+gcloud compute firewall-rules create internal-ssh-ingress --allow=tcp:22 --source-ranges 192.168.10.0/24 --target-tags $INTERNAL_NETWORK_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags juice-shop --tags=$INTERNAL_NETWORK_TAG --zone=$ZONE
+ 
+
+timeout 45 gcloud compute ssh bastion --zone=$ZONE --project=$DEVSHELL_PROJECT_ID --quiet --command="gcloud compute ssh juice-shop --zone=$ZONE --internal-ip --quiet"
+
