65bf42d7e997b65f6f25d5a6990cfeb1c8bcdd05
Techcps <104138529+Techcps@users.noreply.github.com>
2024-12-05 15:22:39 +0530

    Create techcps.sh

--- mini lab : Cloud Storage : 5/techcps.sh
+++ mini lab : Cloud Storage : 5/techcps.sh
@@ -2,5 +2,5 @@
 echo "Please set the below values correctly"
 read -p "Enter the BUCKET_NAME: " BUCKET_NAME
 
-gsutil iam ch -d allUsers gs://$BUCKET_NAME-urgent
+gsutil iam ch -d allUsers gs://$BUCKET_NAME
 
