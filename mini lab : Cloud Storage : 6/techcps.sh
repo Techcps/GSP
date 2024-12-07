01aa2618b5545c1fb1e47692322bbac9b6eaa8c6
Techcps <104138529+Techcps@users.noreply.github.com>
2024-12-07 15:21:03 +0530

    Create techcps.sh

--- mini lab : Cloud Storage : 6/techcps.sh
+++ mini lab : Cloud Storage : 6/techcps.sh
@@ -1,6 +1,6 @@
 
 echo "Please set the below values correctly"
-read -p "Enter the BUCKET: " BUCKET_NAME
+read -p "Enter the BUCKET_NAME: " BUCKET_NAME
 
 curl -LO raw.githubusercontent.com/Techcps/GSP/master/mini%20lab%20%3A%20Cloud%20Storage%20%3A%206/cors-config.json
 
