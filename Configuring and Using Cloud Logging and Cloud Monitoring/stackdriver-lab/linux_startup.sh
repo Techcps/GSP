f795d791472adfd642e38aa9fd1502fce48d547b
Techcps <104138529+Techcps@users.noreply.github.com>
2024-12-19 16:13:19 +0530

    Create linux_startup.sh

--- Configuring and Using Cloud Logging and Cloud Monitoring/stackdriver-lab/linux_startup.sh
+++ Configuring and Using Cloud Logging and Cloud Monitoring/stackdriver-lab/linux_startup.sh
@@ -5,7 +5,12 @@
     apt-get install -y apache2 git
 
     # install Ops Agent
-    
+    curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+    sudo bash add-logging-agent-repo.sh --also-install
+
+    # install monitoring agent
+    curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+    sudo bash add-monitoring-agent-repo.sh --also-install
 
     # download and unpack files
     cd /
